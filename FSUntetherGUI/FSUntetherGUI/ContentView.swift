121bb64869cd41ed9d5b93251c1bf5e4b409ddc1
Ingan121 <lssaa91192@gmail.com>
2022-10-26 02:48:10 +0900

    Add FSUntetherGUI

---
+++ FSUntetherGUI/FSUntetherGUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FSUntetherGUI
+//
+//  Created by 이준서 on 2022/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("FSUntether by Ingan121")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
